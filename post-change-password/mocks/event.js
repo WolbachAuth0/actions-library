12d2c31ca82435f760aa63e417c57e9fbfac2eeb
WolbachAuth0 <aaron.wolbach@okta.com>
2023-12-19 13:22:26 -0500

    add example action post-password-change

---
+++ post-change-password/mocks/event.js
@@ -0,0 +1,38 @@
+module.exports = {
+  connection: {
+    id: 'con_fpe5kj482KO1eOzQ',
+    name: 'Username-Password-Authentication',
+    strategy: 'auth0',
+    metadata: {}
+  },
+  request: {
+    ip: '10.12.13.1',
+    hostname: 'aaron-labs.auth0.com',
+    user_agent: 'curl/7.64.1',
+    method: 'post',
+    geoip: {
+      cityName: 'Bellevue',
+      countryName: 'United States of America',
+      latitude: 47.61793,
+      longitude: -122.19584,
+      continentCode: 'NA',
+      countryCode: 'US',
+      countryCode3: 'USA',
+      subdivisionCode: 'WA',
+      subdivisionName: 'Washington',
+      timeZone: 'America/Los_Angeles'
+    },
+    language: 'en'
+  },
+  tenant: {
+    id: 'aaron-labs'
+  },
+  user: {
+    id: '5f7c8ec7c33c6c004bbafe82',
+    user_id: 'auth0|test12345',
+    email: 'j+smith@example.com',
+    email_verified: true,
+    last_password_reset: '2023-12-19T18:18:07.821Z',
+    username: 'j+smith'
+  }
+}
